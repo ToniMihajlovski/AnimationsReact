f41ca7fd54a96bf9da51a8a7f4fed197407a98a9
ToniMihajlovski <toni@sopro.io>
2020-06-02 11:15:22 +0200

    css added nor working

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 import './Modal.css';
 
-const modal = (props) => (
-    <div className="Modal">
+const modal = (props) => {
+  debugger;
+  const cssClasses = ["Modal", props.show ? 'modalOpen' : 'modalClosed']
+return (
+    <div className={cssClasses.join(' ')}>
         <h1>A Modal</h1>
         <button className="Button" onClick={props.closed}>Dismiss</button>
     </div>
-);
+)};
 
 export default modal;
